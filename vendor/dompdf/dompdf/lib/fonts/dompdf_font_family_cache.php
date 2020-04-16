3158f16157ed27eced51d2ed38ec1f6868fa643c
oussema <oussema.zribi@esprit.tn>
2020-04-16 20:40:57 +0100

    test

---
+++ vendor/dompdf/dompdf/lib/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,89 @@
+<?php return array (
+  'sans-serif' => array(
+    'normal' => $fontDir . '\Helvetica',
+    'bold' => $fontDir . '\Helvetica-Bold',
+    'italic' => $fontDir . '\Helvetica-Oblique',
+    'bold_italic' => $fontDir . '\Helvetica-BoldOblique',
+  ),
+  'times' => array(
+    'normal' => $fontDir . '\Times-Roman',
+    'bold' => $fontDir . '\Times-Bold',
+    'italic' => $fontDir . '\Times-Italic',
+    'bold_italic' => $fontDir . '\Times-BoldItalic',
+  ),
+  'times-roman' => array(
+    'normal' => $fontDir . '\Times-Roman',
+    'bold' => $fontDir . '\Times-Bold',
+    'italic' => $fontDir . '\Times-Italic',
+    'bold_italic' => $fontDir . '\Times-BoldItalic',
+  ),
+  'courier' => array(
+    'normal' => $fontDir . '\Courier',
+    'bold' => $fontDir . '\Courier-Bold',
+    'italic' => $fontDir . '\Courier-Oblique',
+    'bold_italic' => $fontDir . '\Courier-BoldOblique',
+  ),
+  'helvetica' => array(
+    'normal' => $fontDir . '\Helvetica',
+    'bold' => $fontDir . '\Helvetica-Bold',
+    'italic' => $fontDir . '\Helvetica-Oblique',
+    'bold_italic' => $fontDir . '\Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => array(
+    'normal' => $fontDir . '\ZapfDingbats',
+    'bold' => $fontDir . '\ZapfDingbats',
+    'italic' => $fontDir . '\ZapfDingbats',
+    'bold_italic' => $fontDir . '\ZapfDingbats',
+  ),
+  'symbol' => array(
+    'normal' => $fontDir . '\Symbol',
+    'bold' => $fontDir . '\Symbol',
+    'italic' => $fontDir . '\Symbol',
+    'bold_italic' => $fontDir . '\Symbol',
+  ),
+  'serif' => array(
+    'normal' => $fontDir . '\Times-Roman',
+    'bold' => $fontDir . '\Times-Bold',
+    'italic' => $fontDir . '\Times-Italic',
+    'bold_italic' => $fontDir . '\Times-BoldItalic',
+  ),
+  'monospace' => array(
+    'normal' => $fontDir . '\Courier',
+    'bold' => $fontDir . '\Courier-Bold',
+    'italic' => $fontDir . '\Courier-Oblique',
+    'bold_italic' => $fontDir . '\Courier-BoldOblique',
+  ),
+  'fixed' => array(
+    'normal' => $fontDir . '\Courier',
+    'bold' => $fontDir . '\Courier-Bold',
+    'italic' => $fontDir . '\Courier-Oblique',
+    'bold_italic' => $fontDir . '\Courier-BoldOblique',
+  ),
+  'dejavu sans' => array(
+    'bold' => $fontDir . '\DejaVuSans-Bold',
+    'bold_italic' => $fontDir . '\DejaVuSans-BoldOblique',
+    'italic' => $fontDir . '\DejaVuSans-Oblique',
+    'normal' => $fontDir . '\DejaVuSans',
+  ),
+  'dejavu sans mono' => array(
+    'bold' => $fontDir . '\DejaVuSansMono-Bold',
+    'bold_italic' => $fontDir . '\DejaVuSansMono-BoldOblique',
+    'italic' => $fontDir . '\DejaVuSansMono-Oblique',
+    'normal' => $fontDir . '\DejaVuSansMono',
+  ),
+  'dejavu serif' => array(
+    'bold' => $fontDir . '\DejaVuSerif-Bold',
+    'bold_italic' => $fontDir . '\DejaVuSerif-BoldItalic',
+    'italic' => $fontDir . '\DejaVuSerif-Italic',
+    'normal' => $fontDir . '\DejaVuSerif',
+  ),
+  'audiowide' => array(
+    'normal' => $fontDir . '\89a8d6d6ae0a53fe53842c6ad94e97bb',
+  ),
+  'kreon' => array(
+    'normal' => $fontDir . '\bc145546c2c60e27709cf02c740d208f',
+  ),
+  'open sans' => array(
+    'normal' => $fontDir . '\93a3b09bb911df7438d55d4aa77e1bdd',
+  ),
+) ?>
